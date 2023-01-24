07d84aebc6634bfd0606e779fd99ac746e320360
milenalinsb <lins.milena@academico.ifpb.edu.br>
2023-01-24 12:47:33 -0300

    Inicializando projeto

---
+++ api/src/main/java/br/com/guruDelivery/GuruDelivey/GuruDeliveyApplication.java
@@ -0,0 +1,13 @@
+package br.com.guruDelivery.GuruDelivey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GuruDeliveyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GuruDeliveyApplication.class, args);
+	}
+
+}
---
+++ api/src/test/java/br/com/guruDelivery/GuruDelivey/GuruDeliveyApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guruDelivery.GuruDelivey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuruDeliveyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
