b7da91216ca5670f2b85674b1677bd3775b1cd12
jmnj <junior3917@gmail.com>
2023-01-30 15:02:35 -0300

    novo card de produto

--- application/src/components/ProductCard/index.jsx
+++ application/src/components/ProductCard/index.jsx
@@ -1,23 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Counter from '../Counter'
 import Separator from '../Separator'
 
 export default function ProductCard() {
+
+    const [counter, setCounter] = useState(0)
+
   return (
     <div className='max-w-sm bg-surface rounded-lg drop-shadow-md font-sans p-3'>
-        <div className='gap-5 grid grid-cols-4 '>
-            <div className='col-start-1 col-end-3 row-start-1 row-end-3'>
+        <div className='flex flex-col items-center'>
+            <div className=''>
                 <img src='https://bobs.com.br/media/filer_public_thumbnails/filer_public/1f/61/1f61237f-ecaa-4cca-a3ce-c770d5974560/new_v4-bobs-burger-celeb.png__1200x630_subsampling-2_upscale.png'/>
             </div>
-            <div className='col-start-2 col-end-5' >
+            <div className='text-lg' >
                 Name
             </div>
-            <div className='col-start-2 col-end-5 text-sm' >
+            <div className='text-sm mb-2' >
                 asdajsodiaj sdlasdijasodi ajsdoaskdja sodiajsdkada joisdjaksodasj
             </div>
-            <div className='col-start-2 col-end-5 text-end font-bold'>
+            <div className='text-xl font-bold text-primary self-start'>
                 R$ XX,xx
             </div>
-            <Separator className='col-start-1 col-end-5'/>
+            <hr className='w-full mb-2'/>
+        </div>
+        <div className='flex flex-col sm:flex-row justify-between mt-2'>
+                <Counter value={counter} onChange={setCounter}/>
+                <button className='bg-primary basis-2/4 py-1 rounded-md'>Adicionar</button>
         </div>
     </div>
   )
--- application/src/pages/CompPage/index.jsx
+++ application/src/pages/CompPage/index.jsx
@@ -6,13 +6,18 @@ import Separator from '../../components/Separator'
 export default function CompPage() {
   return (
     <div className='flex flex-1 flex-col p-5 bg-background items-center justify-center' >
-      <div className='bg-surface max-w-6xl w-full'>
-        <header className='bg-primary h-48 w-full rounded'>HEADER</header>
-        <Separator/>
-        <div className='px-20'>
+      <div className='bg-surface max-w-6xl w-full rounded-lg'>
+        <header className='bg-primary h-48 w-full flex text-on-primary'>
+          <img src='#'/>
+          <div className='p-2'>
+            <div className='text-2xl'>Nome</div>
+            <div>info</div>
+          </div>
+        </header>
+        <div className='px-20 rounded-md'>
           <InputText placeholder='Pesquisa' />
         </div>
-        <main className='grid sm:grid-cols-3 md:grid-cols-4 bg-background justify-center gap-2 p-2'>
+        <main className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 bg-background justify-center gap-2 p-2'>
           <ProductCard/>
           <ProductCard/>
           <ProductCard/>
